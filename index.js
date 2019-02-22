510655ff536bededf490f48b274d737501f27c15
Richard Lau <riclau@uk.ibm.com>
2019-02-22 01:45:52 +0000

    add End-Of-Life to prohibited strings
    Refs: https://github.com/nodejs/node/pull/26251

--- index.js
+++ index.js
@@ -47,6 +47,7 @@ module.exports.plugins = [
   [
     require("remark-lint-prohibited-strings"),
     [
+      { no: "End-Of-Life", yes: "End-of-Life" },
       { no: "Github", yes: "GitHub" },
       { no: "Javascript", yes: "JavaScript" },
       { no: "Node.JS", yes: "Node.js" },
