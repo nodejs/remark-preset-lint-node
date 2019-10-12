f24b17e3914013b110179acf5a133dead93b0454
Nick Schonning <nschonni@gmail.com>
2019-10-12 00:28:50 -0400

    Use and extend remark-preset-lint-recommended (#21)
    * fix: Disable rules that don't pass from the preset
    
    * feat: Use remark-preset-lint-recommended
    
    * chore: remove rules overlapping preset

--- index.js
+++ index.js
@@ -5,6 +5,8 @@
 // Add in rules alphabetically
 module.exports.plugins = [
   require("remark-lint"),
+  // Leave preset at the top so it can be overridden
+  require("remark-preset-lint-recommended"),
   [require("remark-lint-blockquote-indentation"), 2],
   [
     require("remark-lint-checkbox-character-style"),
@@ -19,31 +21,22 @@ module.exports.plugins = [
   require("remark-lint-fenced-code-flag"),
   [require("remark-lint-fenced-code-marker"), "`"],
   [require("remark-lint-file-extension"), "md"],
-  [require("remark-lint-first-heading-level"), 1],
   require("remark-lint-final-definition"),
-  require("remark-lint-final-newline"),
-  require("remark-lint-hard-break-spaces"),
-  require("remark-lint-hard-break-spaces"),
+  [require("remark-lint-first-heading-level"), 1],
   [require("remark-lint-heading-style"), "atx"],
   [require("remark-lint-list-item-indent"), "space"],
   require("remark-lint-maximum-line-length"),
-  require("remark-lint-no-auto-link-without-protocol"),
-  require("remark-lint-no-blockquote-without-marker"),
   require("remark-lint-no-consecutive-blank-lines"),
-  require("remark-lint-no-duplicate-definitions"),
   require("remark-lint-no-file-name-articles"),
   require("remark-lint-no-file-name-consecutive-dashes"),
   require("remark-lint-no-file-name-outer-dashes"),
-  require("remark-lint-no-heading-content-indent"),
   require("remark-lint-no-heading-indent"),
-  require("remark-lint-no-inline-padding"),
+  [require("remark-lint-no-literal-urls"), false],
   require("remark-lint-no-multiple-toplevel-headings"),
   require("remark-lint-no-shell-dollars"),
-  require("remark-lint-no-shortcut-reference-image"),
   require("remark-lint-no-table-indentation"),
   require("remark-lint-no-tabs"),
   require("remark-lint-no-trailing-spaces"),
-  require("remark-lint-no-unused-definitions"),
   [
     require("remark-lint-prohibited-strings"),
     [
