6fc5006d142233d5ba08ab8d7176243cb63aae35
Derek Lewis <DerekNonGeneric@inf.is>
2020-05-18 16:21:29 -0400

    feat: prohibit missing info string

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ module.exports.plugins = [
   require("remark-lint-checkbox-content-indent"),
   [require("remark-lint-code-block-style"), "fenced"],
   require("remark-lint-definition-spacing"),
-  require("remark-lint-fenced-code-flag"),
+  [require("remark-lint-fenced-code-flag"), { allowEmpty: false }],
   [require("remark-lint-fenced-code-marker"), "`"],
   [require("remark-lint-file-extension"), "md"],
   require("remark-lint-final-definition"),
